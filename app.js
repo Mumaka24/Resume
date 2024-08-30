2206312fd68f59783fca15e9a5c1524675bff48c
Joy <jzembeuk@gmail.com>
2024-08-29 19:31:38 -0500

    Edits

---
+++ app.js
@@ -0,0 +1,18 @@
+const element = document.getElementByClassName("description");
+
+document.body.style.backgroundColor = "yellow";
+description.append()
+
+const pTag = document.querySelectorAll('p');
+
+pTag.innerText = 'This has been changed';
+pTag.style.fontWeight = 'bold'; 
+
+
+pTag.addEventLisner('clicK',()=>{
+    pTag.style.color = 'blue';
+    pTag.innerText = 'The text is now blue';
+});
+
+
+
